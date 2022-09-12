397cd323ce0f36e30193c6c6c3cd0206e401d05f
Yulitooo <emperador1833@gmail.com>
2022-09-12 10:52:42 -0300

    Update constants.php

--- includes/constants.php
+++ includes/constants.php
@@ -1,3 +1,11 @@
 <?php
 
 define('DEST_DIR', str_replace('\\', '/', dirname(__DIR__)) . '/media/');
+
+if (!file_exists(DEST_DIR)) {
+    if (mkdir(DEST_DIR)) {
+        echo 'Target directory ' . DEST_DIR . ' has been created!' . "\n";
+    } else {
+        echo "The fopen function will attempt to create the target directory!\n";
+    }
+}
